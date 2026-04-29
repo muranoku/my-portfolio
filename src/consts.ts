ca88a7e910dbe62af25d3176997b31e238a08cef
AyumuNomura <ayumu@AyumuNomuranoMacBook-Air.local>
2026-04-29 16:21:10 +0900

    [add] タイトル名更新

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'AYUMU Nomura（野村 歩夢）';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
